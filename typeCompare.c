50bfae2769b5bd9566ab179e67970f2b40534f19
yanichik <brunshte@gmail.com>
2018-12-20 11:17:13 -0800

    now works in optimal case. DEL key still needs work

--- typeCompare.c
+++ typeCompare.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ncurses.h>
+#include <ctype.h>
 
 #define ARR 94
+#define ESC 27
 
 long int lineSize(int line, FILE *fp);
 long int fileSize(FILE *fp);
@@ -12,7 +14,8 @@ int main(){
 	echo();
 	FILE *fp = fopen("./challenges/samples/plato.txt", "r");
 	char ch;
-	long int i = 0, j = 0, lines, flength, charcnt = 0, lnsize;
+	long int i = 0, j = 0, flength, charcnt = 0, lnsize;
+	//~ long int lines;
 	int x_cur = 0, y_cur = 0;
 	while ((ch = fgetc(fp)) != EOF){
 		if (ch == '\n'){
@@ -21,10 +24,10 @@ int main(){
 		}
 		printw("%c", ch);
 	}
-	x_cur = i+2;
+	x_cur = i+5;
 	//~ fseek(fp, 0L, SEEK_END);
 	flength = fileSize(fp);
-	lines = i;
+	//~ lines = i;
 	// initialize typing guide
 	i=0;
 	lnsize = lineSize(i, fp);
@@ -34,6 +37,8 @@ int main(){
 	do
 	{
 		ch = getch();
+		if (ch == ESC)
+			mvdelch(x_cur-1, y_cur);
 		if ( ch == 127 ){
 			if(j > 0){ // if text not at beg of line
 				mvdelch(i, j--);
@@ -57,41 +62,76 @@ int main(){
 				}
 				else ;
 			}
-			else if (j == 0 && i == 0){ // curr at beg of doc
+			else if (j == 0 && i == 0) // curr at beg of doc
 				continue;
-			}
 		}
-		else {
-			if (i <= lines)
-			{
+		else if((isalnum(ch) || isspace(ch) || ispunct(ch)) && (ch != ESC)){
+			//~ if (i <= lines)
+			//~ {
 				if (ch != '\n')
 				{
-					mvdelch(i+1, j-1);
-					if (i == 0)
-						move(i+1, j);
-					else move(i+2, j);
-					printw("%c", ARR);
-					j++;
+					if (i == 0){
+						mvdelch(i+1, j);
+						move(i+1, ++j);
+						printw("%c", ARR);
+						move(x_cur, ++y_cur);
+						charcnt++;
+					}
+					else {
+						mvdelch(i+2, j);
+						move(i+2, ++j);
+						printw("%c", ARR);
+						move(x_cur, ++y_cur);
+						charcnt++;
+					}
 				}
 				else if (ch == '\n'){
-					mvdelch(i+1, j-1);
-					i++;
-					move(i+2, 0);
-					printw("%c", ARR);
-					j++;					
+					if (i == 0 && j == lnsize){ // if on 0th line and at end of line
+						mvdelch(i+1, j);
+						j = 0;
+						move(i+3, j);
+						i++;
+						printw("%c", ARR);
+						y_cur = 0;
+						move(++x_cur, y_cur);
+						//~ charcnt++;
+					}
+					else if (i > 0 && j == lnsize+1){ // if any end of any line except 0th
+						mvdelch(i+1, j-1);
+						i++;
+						j = 0;
+						move(i+2, j);
+						printw("%c", ARR);
+						move(x_cur, y_cur+1);
+						//~ charcnt++;
+					}
+					else {
+						mvdelch(i+1, j-1);
+						if (i==0){
+							move(i+1, j);
+							printw("%c", ARR);
+						}
+						else {
+							move(i+2, j);
+							printw("%c", ARR);
+						}
+						x_cur++;
+						y_cur = 0;
+						move(x_cur, y_cur);
+						//~ charcnt++;
+					}					
 				}
-				else if (j == lnsize){
-					j = 0;
-					i++;
-					lnsize = lineSize(i, fp);
-				}
-			}
-			charcnt++;
-			if (ch == '\n')
-				move(y_cur+1, 0);
-			else move(y_cur, x_cur+1);
+				//~ else if (j == lnsize){
+					//~ j = 0;
+					//~ i++;
+					//~ lnsize = lineSize(i, fp);
+				//~ }
+			//~ }
+			//~ if (ch == '\n')
+				//~ move(y_cur+1, 0);
+			//~ else move(y_cur, x_cur+1);
 		}
-	} while (charcnt < flength);
+	} while (ch != ESC);
 	printw("\n%d", i);
 	return 0;
 }
@@ -133,4 +173,3 @@ long int fileSize(FILE *fp){
 	}
 	return size;
 }
-	
